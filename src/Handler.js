d6c9ba846f9cbd71dd3daa460eb90f2d4d2d7148
Shivam Gupta <shivamgupta02005@gmail.com>
2024-01-26 22:58:10 +0530

    Project name and description can now be edited

--- src/Handler.js
+++ src/Handler.js
@@ -1,37 +1,38 @@
-class Handler{
-    constructor(){
-        this._projects = [];
+class Handler {
+  constructor() {
+    this._projects = [];
 
-        this._loadProjects();
-    }
+    this._loadProjects();
+  }
 
-    // PRIVATE METHODS
+  // PRIVATE METHODS
 
-    _getActiveProject(){
-        const [activeProject] = this._projects.filter(project=>project.active);
-        return activeProject;
-    }
+  _getActiveProject() {
+    const [activeProject] = this._projects.filter((project) => project.active);
+    return activeProject;
+  }
 
-    _loadProjects(){
-        const projectsList = document.getElementById('projects-list');
-        projectsList.innerHTML = '';
-        this._projects.forEach(project=>{
-            const div = document.createElement('div');
-            div.className = 'project-item';
-            div.setAttribute('data-id', project.id);
-            div.innerHTML = `
+  _loadProjects() {
+    const projectsList = document.getElementById('projects-list');
+    projectsList.innerHTML = '';
+    this._projects.forEach((project) => {
+      const div = document.createElement('div');
+      div.className = 'project-item';
+      div.setAttribute('data-id', project.id);
+      div.innerHTML = `
                 ${project.name}
                 <i class="fas fa-trash delete-project"></i>
-            `
-            projectsList.appendChild(div);
-        });
-    }
+            `;
+      projectsList.appendChild(div);
+    });
+  }
 
-    _displayTask(task){
-        const div = document.createElement('div');
-        div.className = 'task';
-        div.setAttribute('data-id', task.id);
-        div.innerHTML = task.done?`
+  _displayTask(task) {
+    const div = document.createElement('div');
+    div.className = 'task';
+    div.setAttribute('data-id', task.id);
+    div.innerHTML = task.done
+      ? `
             <div class="task-left">
                 <div class="checkbox done"><i class="fas fa-thin fa-check"></i></div>
                 <div class="task-name">${task.name}</div>
@@ -41,7 +42,8 @@ class Handler{
                 <i class="fas fa-edit task-edit"></i>
                 <i class="fas fa-trash task-delete"></i>
             </div>
-        `: `
+        `
+      : `
             <div class="task-left">
                 <div class="checkbox"></div>
                 <div class="task-name">${task.name}</div>
@@ -51,90 +53,105 @@ class Handler{
                 <i class="fas fa-edit task-edit"></i>
                 <i class="fas fa-trash task-delete"></i>
             </div>
-        `
-        //Making sure that the add task dialog doesn't show up at the top of all tasks
-        const addTaskDialog = document.querySelector('.add-task');
-        document.getElementById('tasks-container').insertBefore(div, addTaskDialog);
-    }
+        `;
+    //Making sure that the add task dialog doesn't show up at the top of all tasks
+    const addTaskDialog = document.querySelector('.add-task');
+    document.getElementById('tasks-container').insertBefore(div, addTaskDialog);
+  }
 
-    _loadActiveProjectTasks(){
-        const activeProject = this._getActiveProject();
-        const tasksContainer = document.getElementById('tasks-container');
+  _loadActiveProjectTasks() {
+    const activeProject = this._getActiveProject();
+    const tasksContainer = document.getElementById('tasks-container');
 
-        //If the task container already has any tasks then this will remove them
-        const tasks = document.querySelectorAll('.task');
-        Array.from(tasks).forEach(task=>tasksContainer.removeChild(task));
+    //If the task container already has any tasks then this will remove them
+    const tasks = document.querySelectorAll('.task');
+    Array.from(tasks).forEach((task) => tasksContainer.removeChild(task));
 
-        //Displaying Active Projects's tasks
-        activeProject.tasks.forEach(task=>this._displayTask(task));
-    }
+    //Displaying Active Projects's tasks
+    activeProject.tasks.forEach((task) => this._displayTask(task));
+  }
 
-    _displayActiveProjectInfo(){
-        document.getElementById('project-info-placeholder').style.display = 'none';
-        document.getElementById('project-info-container').style.display = 'flex';
-        document.getElementById('tasks-container').style.display = 'block';
-        document.getElementById('add-task-button').style.display = 'inline-block';
-        const activeProject = this._getActiveProject();
-        const projectInfo = document.getElementById('project-info');
-        projectInfo.innerHTML = `
+  _displayActiveProjectInfo() {
+    document.getElementById('project-info-placeholder').style.display = 'none';
+    document.getElementById('project-info-container').style.display = 'flex';
+    document.getElementById('tasks-container').style.display = 'block';
+    document.getElementById('add-task-button').style.display = 'inline-block';
+    const activeProject = this._getActiveProject();
+    const projectInfo = document.getElementById('project-info');
+    projectInfo.innerHTML = `
             <div id="project-name-main">
                 ${activeProject.name}    
             </div>
             <div id="project-description-main">
                 ${activeProject.description}
             </div>
-        `
-    }
+        `;
+  }
 
-    // PUBLIC METHODS
+  // PUBLIC METHODS
 
-    setActiveProject(id){
-        this._projects.forEach(project => {
-            if (project.id == id){
-                project.active = true;
-            }else{
-                project.active = false;
-            }
-        })
-        this._displayActiveProjectInfo();
-        this._loadActiveProjectTasks();
-    }
+  setActiveProject(id) {
+    this._projects.forEach((project) => {
+      if (project.id == id) {
+        project.active = true;
+      } else {
+        project.active = false;
+      }
+    });
+    this._displayActiveProjectInfo();
+    this._loadActiveProjectTasks();
+  }
 
-    addProject(project){
-        this._projects.push(project);
-        this.setActiveProject(project.id);
-        this._loadProjects();
-    }
+  addProject(project) {
+    this._projects.push(project);
+    this.setActiveProject(project.id);
+    this._loadProjects();
+  }
 
-    deleteProject(id){
-        this._projects = this._projects.filter(project=>project.id!=id);
-        if (this._projects.length > 0){
-            this.setActiveProject(this._projects[0].id);
-        }else{
-            document.getElementById('project-info-placeholder').style.display = 'block';
-            document.getElementById('project-info-container').style.display = 'none';
-            document.getElementById('tasks-container').style.display = 'none';
-            document.getElementById('add-task-button').style.display = 'none';
-        }
-    }
+  editProject(name, description) {
+    const activeProject = this._getActiveProject();
+    activeProject.changeName(name);
+    activeProject.changeDescription(description);
+    this._displayActiveProjectInfo();
+    this._loadProjects();
+  }
 
-    addTask(task){
-        const activeProject = this._getActiveProject();
-        activeProject.addTask(task);
-        this._displayTask(task);
-    }
+  setProjectForEditing() {
+    const activeProject = this._getActiveProject();
+    document.getElementById('edit-project-name').value = activeProject.name;
+    document.getElementById('edit-project-description').value =
+      activeProject.description;
+  }
 
-    checkbox(id, marked){
-        const activeProject = this._getActiveProject();
-        const [task] = activeProject.tasks.filter(task=>task.id==id);
-        task.done = !marked;
+  deleteProject(id) {
+    this._projects = this._projects.filter((project) => project.id != id);
+    if (this._projects.length > 0) {
+      this.setActiveProject(this._projects[0].id);
+    } else {
+      document.getElementById('project-info-placeholder').style.display =
+        'block';
+      document.getElementById('project-info-container').style.display = 'none';
+      document.getElementById('tasks-container').style.display = 'none';
+      document.getElementById('add-task-button').style.display = 'none';
     }
+  }
 
-    deleteTask(id){
-        const activeProject = this._getActiveProject();
-        activeProject.deleteTask(id);
-    }
-    
+  addTask(task) {
+    const activeProject = this._getActiveProject();
+    activeProject.addTask(task);
+    this._displayTask(task);
+  }
+
+  checkbox(id, marked) {
+    const activeProject = this._getActiveProject();
+    const [task] = activeProject.tasks.filter((task) => task.id == id);
+    task.done = !marked;
+  }
+
+  deleteTask(id) {
+    const activeProject = this._getActiveProject();
+    activeProject.deleteTask(id);
+  }
 }
 
 export default Handler;
--- src/index.js
+++ src/index.js
@@ -1,142 +1,201 @@
 import './css/style.css';
 import Handler from './Handler';
-import {Project, Task} from './Item';
+import { Project, Task } from './Item';
 
-class App{
-    constructor(){
-        this._handler = new Handler();
+class App {
+  constructor() {
+    this._handler = new Handler();
 
-        document.getElementById('add-project-button').addEventListener('click', this._showAddProjectDialog);
-        document.getElementById('add-project-cancel-button').addEventListener('click', this._closeAddProjectDialog);
-        document.getElementById('add-project-dialog-submit-button').addEventListener('click', this._addProject.bind(this));
-        document.getElementById('add-task-button').addEventListener('click', this._showAddTaskDialog);
-        document.querySelector('.add-task-cancel-button').addEventListener('click', this._hideAddTaskDialog);
-        document.querySelector('.add-task-submit-button').addEventListener('click', this._addTask.bind(this));
-        document.getElementById('button-open-nav').addEventListener('click', this._openAndCloseNav);
-        document.getElementById('projects-list').addEventListener('click', this._handleProjectClick.bind(this));
-        document.getElementById('tasks-container').addEventListener('click', this._handleTaskClick.bind(this));
+    document
+      .getElementById('add-project-button')
+      .addEventListener('click', this._showAddProjectDialog);
+    document
+      .getElementById('add-project-cancel-button')
+      .addEventListener('click', this._closeAddProjectDialog);
+    document
+      .getElementById('add-project-dialog-submit-button')
+      .addEventListener('click', this._addProject.bind(this));
+    document
+      .getElementById('add-task-button')
+      .addEventListener('click', this._showAddTaskDialog);
+    document
+      .querySelector('.add-task-cancel-button')
+      .addEventListener('click', this._hideAddTaskDialog);
+    document
+      .querySelector('.add-task-submit-button')
+      .addEventListener('click', this._addTask.bind(this));
+    document
+      .getElementById('button-open-nav')
+      .addEventListener('click', this._openAndCloseNav);
+    document
+      .getElementById('projects-list')
+      .addEventListener('click', this._handleProjectClick.bind(this));
+    document
+      .getElementById('tasks-container')
+      .addEventListener('click', this._handleTaskClick.bind(this));
+    document
+      .getElementById('edit-project-info-button')
+      ?.addEventListener('click', this._handleEditProjectInfo.bind(this));
+    document
+      .getElementById('edit-project-cancel-button')
+      ?.addEventListener('click', this._cancelEditProjectInfo);
+    document
+      .getElementById('edit-project-submit-button')
+      ?.addEventListener('click', this._submitEditProjectInfo.bind(this));
+  }
+
+  _showAddProjectDialog() {
+    document.getElementById('add-project-window-container').style.display =
+      'flex';
+  }
+
+  _closeAddProjectDialog() {
+    document.getElementById('add-project-window-container').style.display =
+      'none';
+  }
+
+  _addProject() {
+    const name = document.getElementById('project-name');
+    const description = document.getElementById('project-description');
+    if (!name.value || !description.value) {
+      alert('Please enter name and description');
+      return;
     }
+    const project = new Project(name.value, description.value);
+    this._handler.addProject(project);
 
-    _showAddProjectDialog(){
-        document.getElementById('add-project-window-container').style.display = 'flex';
+    this._closeAddProjectDialog();
+    name.value = '';
+    description.value = '';
+  }
+
+  _showAddTaskDialog() {
+    document.querySelector('.add-task').style.display = 'flex';
+    document.getElementById('add-task-button').style.display = 'none';
+  }
+
+  _hideAddTaskDialog() {
+    document.querySelector('.add-task').style.display = 'none';
+    document.getElementById('add-task-button').style.display = 'inline-block';
+  }
+
+  _addTask() {
+    const name = document.querySelector('.add-task-name');
+    const dueDate = document.querySelector('.add-task-date');
+    if (!name.value || !dueDate.value) {
+      alert('Please enter name and due date');
+      return;
     }
+    const task = new Task(name.value, dueDate.value);
+    this._handler.addTask(task);
+    name.value = '';
+    dueDate.value = '';
+    this._hideAddTaskDialog();
+  }
 
-    _closeAddProjectDialog(){
-        document.getElementById('add-project-window-container').style.display = 'none';
+  _handleProjectClick(e) {
+    //Deleting Project
+    if (e.target.classList.contains('delete-project')) {
+      const projectId = e.target.parentElement.dataset.id;
+      this._handler.deleteProject(projectId);
+      e.target.parentElement.remove();
+      return;
     }
-
-    _addProject(){
-        const name = document.getElementById('project-name');
-        const description = document.getElementById('project-description');
-        if (!name.value || !description.value){
-            alert('Please enter name and description');
-            return;
-        }
-        const project = new Project(name.value,description.value);
-        this._handler.addProject(project);
-
-        this._closeAddProjectDialog();
-        name.value = '';
-        description.value = '';
+    //Switching from one Project to another
+    if (e.target.classList.contains('project-item')) {
+      const projectId = e.target.dataset.id;
+      this._handler.setActiveProject(projectId);
+      return;
     }
+  }
 
-    _showAddTaskDialog(){
-        document.querySelector('.add-task').style.display = 'flex';
-        document.getElementById('add-task-button').style.display = 'none';
+  _handleTaskClick(e) {
+    //Marking and unmarking task as done
+    if (
+      e.target.classList.contains('checkbox') ||
+      e.target.classList.contains('fa-check')
+    ) {
+      this._checkbox(e);
+      return;
     }
-
-    _hideAddTaskDialog(){
-        document.querySelector('.add-task').style.display = 'none';
-        document.getElementById('add-task-button').style.display = 'inline-block';
+    //Deleting task
+    if (e.target.classList.contains('task-delete')) {
+      this._deleteTask(e);
+      return;
     }
+  }
 
-    _addTask(){
-        const name = document.querySelector('.add-task-name');
-        const dueDate = document.querySelector('.add-task-date');
-        if (!name.value || !dueDate.value){
-            alert('Please enter name and due date');
-            return;
-        }
-        const task = new Task(name.value, dueDate.value)
-        this._handler.addTask(task);
-        name.value = '';
-        dueDate.value = '';
-        this._hideAddTaskDialog();
+  _deleteTask(e) {
+    const id = e.target.parentElement.parentElement.dataset.id;
+    e.target.parentElement.parentElement.remove();
+    this._handler.deleteTask(id);
+  }
+
+  _checkbox(e) {
+    if (e.target.classList.contains('checkbox')) {
+      const id = e.target.parentElement.parentElement.dataset.id;
+      const marked = true ? e.target.innerHTML != '' : false;
+      if (marked) {
+        e.target.firstElementChild.remove();
+        e.target.classList.remove('done');
+      } else {
+        e.target.innerHTML = `<i class="fas fa-thin fa-check"></i>`;
+        e.target.classList.add('done');
+      }
+      this._handler.checkbox(id, marked);
+      return;
     }
-
-    _handleProjectClick(e){
-        //Deleting Project
-        if (e.target.classList.contains('delete-project')){
-            const projectId = e.target.parentElement.dataset.id;
-            this._handler.deleteProject(projectId);
-            e.target.parentElement.remove();
-            return;
-        }
-        //Switching from one Project to another 
-        if (e.target.classList.contains('project-item')){
-            const projectId = e.target.dataset.id;
-            this._handler.setActiveProject(projectId);
-            return;
-        }
+    if (e.target.classList.contains('fa-check')) {
+      const id = e.target.parentElement.parentElement.parentElement.dataset.id;
+      const marked = true;
+      e.target.parentElement.classList.remove('done');
+      e.target.remove();
+      this._handler.checkbox(id, marked);
+      return;
     }
+  }
 
-    _handleTaskClick(e){
-        //Marking and unmarking task as done
-        if (e.target.classList.contains('checkbox') || e.target.classList.contains('fa-check')){
-            this._checkbox(e);
-            return;
-        }
-        //Deleting task
-        if (e.target.classList.contains('task-delete')){
-            this._deleteTask(e);
-            return;
-        }
+  _handleEditProjectInfo() {
+    document.getElementById('project-info-container').style.display = 'none';
+    document.getElementById('edit-project-info-container').style.display =
+      'flex';
+    this._handler.setProjectForEditing();
+  }
 
+  _cancelEditProjectInfo() {
+    document.getElementById('project-info-container').style.display = 'flex';
+    document.getElementById('edit-project-info-container').style.display =
+      'none';
+    document.getElementById('edit-project-name').value = '';
+    document.getElementById('edit-project-description').value = '';
+  }
+
+  _submitEditProjectInfo() {
+    const name = document.getElementById('edit-project-name').value;
+    const description = document.getElementById(
+      'edit-project-description'
+    ).value;
+    if (!name || !description) {
+      alert('Please enter name and description');
+      return;
     }
+    this._handler.editProject(name, description);
+    this._cancelEditProjectInfo();
+  }
 
-    _deleteTask(e){
-        const id = e.target.parentElement.parentElement.dataset.id;
-        e.target.parentElement.parentElement.remove();
-        this._handler.deleteTask(id);
+  _openAndCloseNav() {
+    const sidebar = document.getElementById('sidebar');
+    const mainPanel = document.getElementById('main-panel');
+    if (sidebar.classList.contains('open')) {
+      mainPanel.style.width = '75vw';
+      sidebar.style.display = 'inline-block';
+      sidebar.classList.remove('open');
+    } else {
+      sidebar.style.display = 'none';
+      mainPanel.style.width = '100vw';
+      sidebar.classList.add('open');
     }
-
-    _checkbox(e){
-        if (e.target.classList.contains('checkbox')){
-            const id = e.target.parentElement.parentElement.dataset.id;
-            const marked = true?e.target.innerHTML != "" : false;
-            if (marked){
-                e.target.firstElementChild.remove();
-                e.target.classList.remove('done');
-            }else{
-                e.target.innerHTML = `<i class="fas fa-thin fa-check"></i>`;
-                e.target.classList.add('done');
-            }
-            this._handler.checkbox(id,marked);
-            return;
-        }
-        if (e.target.classList.contains('fa-check')){
-            const id = e.target.parentElement.parentElement.parentElement.dataset.id;
-            const marked = true;
-            e.target.parentElement.classList.remove('done');
-            e.target.remove();
-            this._handler.checkbox(id,marked);
-            return;
-        }
-    }
-
-    _openAndCloseNav(){
-        const sidebar = document.getElementById('sidebar');
-        const mainPanel = document.getElementById('main-panel');
-        if (sidebar.classList.contains('open')){
-            mainPanel.style.width = '75vw';
-            sidebar.style.display = 'inline-block';
-            sidebar.classList.remove('open')
-        }else{
-            sidebar.style.display = 'none';
-            mainPanel.style.width = '100vw';
-            sidebar.classList.add('open');
-        }
-    }
+  }
 }
 
 const app = new App();
